74004b2119caf012113b45b14064519713a0ed04
malekak44 <malekak44@gmail.com>
2021-08-28 21:28:39 +0600

    hero section done

--- app/js/script.js
+++ app/js/script.js
@@ -2,24 +2,29 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const fadeElems = document.querySelectorAll('.has-fade');
+const submitBtn = document.querySelector("#submitBtn");
 
-btnHamburger.addEventListener('click', function(){
-  if(header.classList.contains('open')){ // Close Hamburger Menu 
+btnHamburger.addEventListener('click', function () {
+  if (header.classList.contains('open')) { // Close Hamburger Menu 
     body.classList.remove('noscroll');
-    header.classList.remove('open');    
-    fadeElems.forEach(function(element){
+    header.classList.remove('open');
+    fadeElems.forEach(function (element) {
       element.classList.remove('fade-in');
       element.classList.add('fade-out');
     });
-    
+
   }
   else { // Open Hamburger Menu
     body.classList.add('noscroll');
     header.classList.add('open');
-    fadeElems.forEach(function(element){
+    fadeElems.forEach(function (element) {
       element.classList.remove('fade-out');
       element.classList.add('fade-in');
     });
 
-  }  
+  }
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 });
